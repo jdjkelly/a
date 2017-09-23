4923681935f188c26b49ce9b0f62f73d7f508692
Joshua Kelly <jdjkelly@gmail.com>
2017-09-23 14:09:01 -0400

    erc20 started

--- 02_erc20_token/Token.sol
+++ 02_erc20_token/Token.sol
@@ -1,19 +1,29 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.13
 
-contract MoustacheCoin {
-  event Transfer(address indexed from, address indexed to, uint256 value);
+// https://theethereum.wiki/w/index.php/ERC20_Token_Standard
+contract BeardCoin {
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
   mapping (address => uint256) public balanceOf;
-  string public name;
-  string public symbol;
-  uint8 public decimals;
+  string public constant name = "BeardCoin";
+  string public constant symbol = "BRD";
+  uint8 public constant decimals = 10;
+  uint256 public constant totalSupply = 9999999999;
 
-  function MoustacheCoin(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits)
+  function BeardCoin(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits)
   {
     balanceOf[msg.sender] = initialSupply;
     name = tokenName;
     symbol = tokenSymbol;
     decimals = decimalUnits;
   }
+
+  function totalSupply() constant returns (uint totalSupply);
+  function balanceOf(address _owner) constant returns (uint balance);
+  function transfer(address _to, uint _value) returns (bool success);
+  function transferFrom(address _from, address _to, uint _value) returns (bool success);
+  function approve(address _spender, uint _value) returns (bool success);
+  function allowance(address _owner, address _spender) constant returns (uint remaining);
 
   function transfer(address _to, uint256 _value) {
     require(balanceOf[msg.sender] >= _value);
